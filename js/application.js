897aea05253e3656101e10615505ff1b913316e1
= <coreydavidbaker@gmail.com>
2014-01-14 11:18:41 -0600

    Added the baseMap div as well as some basic js to call the map

--- js/application.js
+++ js/application.js
@@ -1,3 +1,21 @@
 //created by Corey Baker
 //using version 3.8 of Esri's JS API
 //build using ok coding practices and lots of heart
+
+var baseMap;
+
+require (["esri/map", "dojo/domReady!"],
+
+function(Map){
+
+baseMap = new Map("esriMap",{
+	center: [-95.643783, 38.729942],
+	zoom: 4, 
+	basemap: "gray"
+}); //end basemap
+
+
+
+
+
+}); //end require statement
