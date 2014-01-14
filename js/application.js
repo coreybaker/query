f791fdcdaa314e9ee3d42686ea225ca18651fe79
= <coreydavidbaker@gmail.com>
2014-01-14 11:32:26 -0600

    added service from sample server of counties.  It draws really low, probably change layer visibility until I query later

--- js/application.js
+++ js/application.js
@@ -4,15 +4,35 @@
 
 var baseMap;
 
-require (["esri/map", "dojo/domReady!"],
+require (["esri/map","esri/layers/FeatureLayer",
+"dojo/domReady!"
 
-function(Map){
+
+	],
+
+function(Map, FeatureLayer){
 
 baseMap = new Map("esriMap",{
 	center: [-95.643783, 38.729942],
 	zoom: 4, 
 	basemap: "gray"
 }); //end basemap
+
+
+var demographicFeatureLayer = "http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Census_USA/MapServer/3";
+
+var demographicLayerConstruct = new FeatureLayer(demographicFeatureLayer, {
+	mode: FeatureLayer.MODE_ONDEMAND, 
+	displayOnPan: false, 
+	outFields: ["*"],
+	id: "2000 Demographics",
+
+}); //end featurelayer construction
+
+
+//adds 
+
+baseMap.addLayer(demographicLayerConstruct);
 
 
 
